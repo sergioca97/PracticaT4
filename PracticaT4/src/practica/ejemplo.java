d4cfad8953604e1b34ceffbb698ef0166cf6dd3f
Juan Antonio Redondo <35599027+JuanAntonioRC@users.noreply.github.com>
2018-03-09 13:32:39 +0100

    Update ejemplo.java

--- PracticaT4/src/practica/ejemplo.java
+++ PracticaT4/src/practica/ejemplo.java
@@ -5,6 +5,7 @@ public class ejemplo
 public static void main(String[]args)
 {
 		//TODO Auto-generated method stub
+	//he comentado (juan antonio redondo)
 		System.out.println("Hola Mundo!");
 		System.out.println("Nueva línea de cambio de código");
 		System.out.println("Cambio en el código desde repositorio online");
